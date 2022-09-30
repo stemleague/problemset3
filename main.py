a66cf5a82cfa87796ce0c0c6aeb16fdfa3c6bdb5
stem league <64894084+stemleague@users.noreply.github.com>
2022-09-29 18:58:52 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+# Crack the Code: Problem Set 2
+
+# Problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# STEP 1: Clarify your questions and assumptions
+#
+# TODO: Write 3 questions you have about the given problem below in the commented section:
+#
+# Question 1:
+# Question 2:
+# Question 3:
+
+
+
+# STEP 2: Whiteboarding
+#
+# TODO: Write 5 test cases you expect reverse_string() should output (Ex: reverse_string("hi") --> "ih"). Get 
+# creative - what if we get empty string? What about casing of the string?
+#
+# Test Case 1:
+# Test Case 2:
+# Test Case 3:
+#
+# TODO: Write your Pseudocode below:
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+# STEP 3: Code it up!
+# TODO: Write your implementation below. 
+def two_sum(nums, target):
+
+
+
+
+
+# STEP 4: Testing Your Code
+# Convert your above test cases from Step 2 into actual function calls and run your code to check if your function works.
+
+
+# STEP 5: Optimize Your Code
+# TODO: There is no correct answer, but out of your best guess, what is your code's time complexity? 
+#
+#
